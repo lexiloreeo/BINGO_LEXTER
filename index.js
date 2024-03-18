21244d50ea1040313cb32e0bf3db0cbff6e086b8
jhon lexter vizon <vizonlexter.wup@gmail.com>
2024-03-18 09:14:31 +0800

    lexter igop

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-console.log("Hello World")
+console.log("Hello World!!!")
 let B = false, 
 I = false,
 N = false, 
 G = false,
 O = false;
 
+let BNumber,INumber,NNunmber,GNumber,ONumber;
 function newNumber (maxNumber) {
     // Addition = "+"
     // Subtraction = "-"
@@ -20,26 +21,64 @@ function newNumber (maxNumber) {
     if (x <= 15) {
         console.log(`New number is ${x}  belongs to "B"`);
         B = true;
+        BNumber= x;
     } else if (x >= 16 && x <= 30) {
         console.log(`New number is ${x} belongs to "I"`);
         I = true;
+        INumber= x;
     } else if (x >= 31 && x <= 45) {
         console.log(`New number is ${x} belongs to "N"`);
         N = true;
+        NNunmber= x;
     } else if (x >= 46 && x <= 60) {
         console.log(`New number is ${x} belongs to "G"`);
         G = true;
+        GNumber= x;
+
     } else if (x >= 61 && x <= 75) {
         console.log(`New number is ${x} belongs to "O"`);
         O = true;
+        ONumber= x;
     }
+
 
     if (B == true && I==true && N == true && G == true && O == true) {
-     console.log(`BINGOOOOOOO!!`)   
+     console.log(`BINGOOOOOOO!!`);
+     console.table({
+        B:BNumber,
+        I:BNumber,
+        N:BNumber,
+        G:BNumber,
+        O:BNumber,
+
+
+
+     })
+
+     }
+ 
     }
 
 
-   return console.log(x);
+// newNumber(75);
+//for (let counter = 10; counter >= 0; counter -= 1) {
+   // console.log(Current counter is ${counter});
+//}
+
+let counter = 5
+for (counter; counter >= 0; counter--){
+    console.log(`Current counter is ${counter}`);
+    //if (counter == 2) break;
+    newNumber(75);
 }
 
-newNumber(75);
+// while (counter => 0) {
+//console.log (`While, Current counter is ${counter});
+// if (counter ==2) break;
+// newNumber(75);
+// counter --;
+//}
+
+while (!B || !I || !N || !G || !O) {
+    newNumber(75);
+}
